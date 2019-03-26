de89a79f63f13312ec55082029413cd3fe21b1e5
Kakeru Ishii <i.kakeru@grimoire.gl>
2019-03-26 14:36:38 +0900

    fix password spell

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ app.post(
     if (users[userId] === undefined) {
       users[userId] = {
         nickname: userId,
-        passwd: sha.x2(passwd)
+        password: sha.x2(passwd)
       };
       return res.status(200).json({
         message: "Account successfully created",
